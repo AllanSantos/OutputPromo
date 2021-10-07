fd1804c78f4340ea6528d6b6f2d0348d2365da5c
Allan <allan.jre@gmail.com>
2021-10-07 14:31:06 -0300

    Ajuste de timeout

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import routes from './routes';
-var timeout = require('connect-timeout');
 
 class App {
   constructor() {
@@ -13,12 +12,6 @@ class App {
   }
 
   middlewares() {
-    this.server.use(timeout(120000));
-    this.server.use(haltOnTimedout);
-
-    function haltOnTimedout(req, res, next){
-      if (!req.timedout) next();
-    }
     this.server.use(cors());
     this.server.use(express.json());
   }
